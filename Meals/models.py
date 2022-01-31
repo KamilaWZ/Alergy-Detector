3b60fdd9d6b9cd397e432cb6a9bfc5ea13ec2f72
KamilaWZ <kwalaszczyk.ziomek@gmail.com>
2022-01-31 13:00:54 +0100

    #4 create model, update settings

--- Meals/models.py
+++ Meals/models.py
@@ -2,5 +2,5 @@ from unicodedata import name
 from django.db import models
 
 class Ingredient(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=95)
     allergy_potential = models.IntegerField()
--- alergydetector/settings.py
+++ alergydetector/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'Meals.apps.MealsConfig'
+    
+    'Meals.apps.MealsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
