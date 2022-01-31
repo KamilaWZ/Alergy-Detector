c3b53c4e7f06ea2d83be9e8a3216bbc46feab103
KamilaWZ <kwalaszczyk.ziomek@gmail.com>
2022-01-31 12:09:04 +0100

    #5 setup data base, migration model, update model and setting correction

--- Meals/models.py
+++ Meals/models.py
@@ -3,4 +3,4 @@ from django.db import models
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
-    allergy_potential = models.IntegerField(min_value=1, max_value=5)
+    allergy_potential = models.IntegerField(default=1)
--- alergydetector/settings.py
+++ alergydetector/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'Meals.apps.MealsConfig'
+    'Meals.apps.MealsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
