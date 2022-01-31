cd72854b249fcc50df30b79b86ee89672021069f
KamilaWZ <kwalaszczyk.ziomek@gmail.com>
2022-01-31 10:39:49 +0100

    #3 start app Meals, update settings base datas

--- alergydetector/settings.py
+++ alergydetector/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'alergydetector.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Alergydetector', 
+        'USER': 'postgres', 
+        'PASSWORD': 'Ja1234',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
 }
 
