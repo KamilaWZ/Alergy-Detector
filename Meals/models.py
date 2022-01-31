b63bb620bb7b7aa299b8c7dd6c703562dc7d71d6
KamilaWZ <kwalaszczyk.ziomek@gmail.com>
2022-01-31 11:28:24 +0100

    #4 create ingredient model, settings update

--- Meals/models.py
+++ Meals/models.py
@@ -1,3 +1,6 @@
+from unicodedata import name
 from django.db import models
 
-# Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+    allergy_potential = models.IntegerField(min_value=1, max_value=5)
