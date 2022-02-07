e430619f7d0074fd308ed427c68940ffa8348788
Anna Nowak <matisnape@wp.pl>
2022-02-07 20:22:05 +0100

    Validate ingredient name presence [#5]

--- Meals/models.py
+++ Meals/models.py
@@ -1,6 +1,7 @@
 from unicodedata import name
 from django.db import models
 
+
 class Ingredient(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, blank=False)
     allergy_potential = models.IntegerField(default=1)
