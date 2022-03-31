86a13cd148816f4b5b72e617299d6c052796c084
KamilaWZ <kwalaszczyk.ziomek@gmail.com>
2022-03-31 22:27:03 +0200

    #4 ingredient model creation

--- alergy_detector/models.py
+++ alergy_detector/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.forms import ChoiceField
+import datetime
 
-# Create your models here.
+class Ingredients(models.Model):
+    ingredient = models.CharField(max_length=200, blank=False)
+    potential_choices = ((0,0), (1,1), (2,2), (3,3))
+    alergy_potential = models.PositiveSmallIntegerField(blank=False, choices = potential_choices),
+    eating_date = models.DateTimeField(blank = False)
+
+    def __str__(self):
+            return self.ingredient
