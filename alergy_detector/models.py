3b3ed08e21378ecb59809e2f488bc5032814845c
KamilaWZ <kwalaszczyk.ziomek@gmail.com>
2022-03-31 22:49:39 +0200

    #15 Meal Model create

--- alergy_detector/models.py
+++ alergy_detector/models.py
@@ -1,12 +1,22 @@
+from tkinter import CASCADE
 from django.db import models
 from django.forms import ChoiceField
 import datetime
 
 class Ingredients(models.Model):
-    ingredient = models.CharField(max_length=200, blank=False)
-    potential_choices = ((0,0), (1,1), (2,2), (3,3))
+    ingredient_name = models.CharField(max_length=200, blank=False)
+    potential_choices = ((0, 'none'), (1,'low'), (2,'middle'), (3,'high'))
     alergy_potential = models.PositiveSmallIntegerField(blank=False, choices = potential_choices),
-    eating_date = models.DateTimeField(blank = False)
+    eating_day = models.DateField(blank = False)
+    eating_hour = models.TimeField(blank = False)
+    relation_with = models.ForeignKey('Meals', on_delete=CASCADE)
+    
+    
+class Meal(models.Model):
+    meal_name = models.CharField(max_length=200, blank=False)
+    description = models.TextField(blank = True, default=meal_name)
 
-    def __str__(self):
-            return self.ingredient
+
+def __str__(self):
+        return self.meal_name
+   
