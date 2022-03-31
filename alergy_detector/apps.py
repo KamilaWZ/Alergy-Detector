b2578c753c1bad69c49981cb05dcff14b2b31334
KamilaWZ <kwalaszczyk.ziomek@gmail.com>
2022-03-31 21:25:37 +0200

    #3 start alergy_detector app

---
+++ alergy_detector/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AlergyDetectorConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'alergy_detector'
--- alergydetector/settings.py
+++ alergydetector/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'alergy_detector.app.AlergyDetectorConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
