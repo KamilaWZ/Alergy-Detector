7f7cf3615066fa2a996869a0af521872c565ce63
KamilaWZ <kwalaszczyk.ziomek@gmail.com>
2022-04-12 15:22:22 +0200

    #7 templates for ingredients view

--- alergy_detector/views.py
+++ alergy_detector/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('<h1>ALERGY DETECTOR</h1>')
+    return render(request, 'alergy_detector/home.html')
